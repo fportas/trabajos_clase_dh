74121dd3aa67f990c22815926a55d0fd52558b3c
fportas <portasfederico@gmail.com>
2019-06-03 17:49:32 -0300

    cambios en general y el extra

---
+++ 190602_trabajo_extra_oop/Alumno.php
@@ -0,0 +1,27 @@
+<?php
+
+	class Alumno
+  //los atributos en privado
+	{
+		private $name;
+		private $lastName;
+		private $id;
+
+    // creo la función para construir los atributos
+		public function __construct($theName, $theLastName)
+		{
+			$this->name = $theName;
+			$this->lastName = $theLastName;
+		}
+
+    // los geters
+
+    // el get del nombre
+
+		public function getName() {
+			return $this->name;
+		}
+
+    // el geter del apellido
+
+	}
