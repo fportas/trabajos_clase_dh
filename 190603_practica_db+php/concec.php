d439d5349b8d7e40d9e8a0728a9bba0f6dcf8af2
fportas <portasfederico@gmail.com>
2019-06-05 17:49:33 -0300

    cambios en el ehjercicio de bd de movies

--- 190603_practica_db+php/concec.php
+++ 190603_practica_db+php/concec.php
@@ -4,4 +4,4 @@
 $dsn = "mysql:host=localhost;dbname=movies_db;port=3306";
 $opt = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
-$base = new PDO($dsn, 'root', 'root', $opt); //se conecta a la base de datos y me devuelve un objeto PDO que me sirve para correr consultas en esa base de datos
+$base = new PDO($dsn, 'root', '', $opt); //se conecta a la base de datos y me devuelve un objeto PDO que me sirve para correr consultas en esa base de datos
--- 190603_practica_db+php/films.php
+++ 190603_practica_db+php/films.php
@@ -2,13 +2,15 @@
 require_once 'concec.php';
 
 try {
-	$resultado = $base->query("SELECT * from genres"); //corre la consulta y me devuelve un resultado dentro de un objeto hijo pdo
+	$resultado_generos = $base->query("SELECT * from genres"); //corre la consulta y me devuelve un resultado dentro de un objeto hijo pdo
+	$resultado_movies = $base->query("SELECT * from movies"); //corre la consulta y me devuelve un resultado dentro de un objeto hijo pdo
 
 	//equivalente a la línea de arriba pero usando statements
 	// $resultado = $base->prepare("SELECT * from autores");
 	// $resultado->execute();
 
 } catch(PDOException $error) {
+
 	$mensaje = $error->getMessage(); //string descriptiva del error
 	$codigo = $error->getCode(); //codigo tipificado del error
 
@@ -16,8 +18,11 @@ try {
 	die(); //lo mismo que exit()
 }
 
+$posPeli = $base->query("SELECT genres_id FROM movies WHERE id = 5");
 
-$genres = $resultado->fetchAll(PDO::FETCH_ASSOC); //me convierte el "resultado pdo" en un array de arrays asociativos de php
+
+$genres = $resultado_generos->fetchAll(PDO::FETCH_ASSOC); //me convierte el "resultado pdo" en un array de arrays asociativos de php
+$movies = $resultado_movies->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
@@ -25,7 +30,7 @@ $genres = $resultado->fetchAll(PDO::FETCH_ASSOC); //me convierte el "resultado p
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Géneros películas</title>
+	<title>Listado Pelis con Genénero</title>
 	<meta charset="utf-8">
 	<style type="text/css">
 		body {
@@ -45,7 +50,6 @@ $genres = $resultado->fetchAll(PDO::FETCH_ASSOC); //me convierte el "resultado p
 			display: inline-block;
 			vertical-align:top;
 			width:28%;
-			height: 50px;
 			padding:10px;
 			background-color:#050505;
 			border-radius: 3px;
@@ -54,7 +58,6 @@ $genres = $resultado->fetchAll(PDO::FETCH_ASSOC); //me convierte el "resultado p
 
 		div {
 			margin-bottom:5px;
-			height: 20%;
 		}
 
 	</style>
@@ -64,12 +67,25 @@ $genres = $resultado->fetchAll(PDO::FETCH_ASSOC); //me convierte el "resultado p
 	<!-- <div>
 		<a href="genres.php">Traer todos los generos de las películas</a>
 	</div> -->
-	<?php foreach($genres as $genre) { ?>
+	<?php foreach($movies as $movie) { ?>
 		<article>
 			<div>
-				<label>Genero:</label> <strong><?php echo $genre['name'] ?></strong>
+				<label>Película:</label> <strong><?php echo $movie['title'] ?></strong>
+			</div>
+			<div>
+				<label>Genero:</label> <strong><?php echo $genres[($movie['genre_id'])] ?></strong>
 			</div>
 		</article>
 	<?php } ?>
+
+	<article>
+		<div>
+			<label>Película:</label> <strong><?php echo $movie[5] ?></strong>
+		</div>
+		<div>
+			<label>Genero:</label> <strong><?php echo $posPeli ?></strong>
+		</div>
+	</article>
+
 </body>
 </html>
